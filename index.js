898f4cd43402102d593c13cd8f832860ecc933c8
Arielle Montgomery <Armontgomery07@gmail.com>
2023-05-05 06:28:29 -0400

    finished

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+    const button = document.getElementById('button');
+  
+    function clickAlert() {
+      alert("I'm not the Pillsbury dough boy!");
+    }
+  
+    button.addEventListener('click', clickAlert);
+  }
+  
+  addingEventListener();
